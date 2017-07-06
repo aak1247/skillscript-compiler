bca6acfc93b824bb9408a2c17c570c119c11c490
aak1247 <aak1247@126.com>
2017-07-06 23:46:09 +0800

    add some codes

--- compiler.c
+++ compiler.c
@@ -7,6 +7,16 @@ int token;            // current token
 char *src, *old_src;  // pointer to source code string;
 int poolsize;         // default size of text/data/stack
 int line;             // line number
+int *text,              //text segment
+    *old_text,          //dump text segment
+    *stack;             //stack
+char *data;             //data stack
+int *pc, *bp, *sp, ax, cycle;   //register
+
+enum{
+    ADD,    //add
+
+}
 
 void next() {
     token = *src++;
@@ -57,6 +67,30 @@ int main(int argc, char **argv)
     }
     src[i] = 0; // add EOF character
     close(fd);
+
+    //allocate memory for virture machine
+    if (!(text = old_text = malloc(poolsize))){
+        printf("could not malloc %d for text area ", poolsize);
+        return -1;
+    }
+
+    if (!(data = malloc(poolsize))){
+        printf("could not malloc %d for data area ", poolsize);
+        return -1;
+    }
+
+    if (!(stack = malloc(poolsize))){
+        printf("could not malloc %d for stack area ", poolsize);
+        return -1;
+    }
+
+    memset(text, 0, poolsize);
+    memset(data, 0, poolsize);
+    memset(stack, 0, poolsize);
+
+    bp = sp =(int*)((int)stack + poolsize);
+    ax = 0;
+
 
     program();
     return eval();
